37bcc694a24734194fd8d4501a34b5d2b6f74a4a
nickwlong <nickwlong@gmail.com>
2022-08-12 10:49:03 +0100

    refactored dish calling methods and dish printing methods

---
+++ spec/Menu_spec.rb
@@ -0,0 +1,39 @@
+require "Menu"
+
+RSpec.describe Menu do
+    describe "#add and #dishes_of_category method" do
+        it "Checks only starter dishes are returned" do
+            testmenu = Menu.new
+            fakedish = Dish.new("fakedish", "7.50", "starter")
+            testmenu.add_dish("fakedish", "7.50", "starter")
+            testmenu.add_dish("fakedish2","5", "main")
+            
+            expect(testmenu.dishes_of_category("starter")).to eq [fakedish]
+        end
+        it "Checks all dishes are returned" do
+            testmenu = Menu.new
+            fakedish = Dish.new("fakedish", "7.50", "starter")
+            fakedish2 = Dish.new("fakedish2","5", "main")
+            testmenu.add_dish("fakedish", "7.50", "starter")
+            testmenu.add_dish("fakedish2","5", "main")
+            
+            expect(testmenu.dishes_of_category).to eq [fakedish, fakedish2]
+        end
+    end
+    describe "#start_list_print method" do
+        it "Checks a list of formatted strings of starters is returned" do
+            testmenu = Menu.new
+            testmenu.add_dish("fakedish", "7.50", "starter")
+            testmenu.add_dish("fakedish2", "5", "main")
+            
+            expect(testmenu.print_dishes_with_prices("starter")).to eq ["fakedish: £7.50\n"]
+        end
+        it "Checks a list of formatted strings of starters is returned - multiple" do
+            testmenu = Menu.new
+            testmenu.add_dish("fakedish", "7.50", "starter")
+            testmenu.add_dish("fakedish2", "5", "starter")
+            
+            expect(testmenu.print_dishes_with_prices("starter")).to eq ["fakedish: £7.50\n", "fakedish2: £5\n"]
+        end
+    end
+end
