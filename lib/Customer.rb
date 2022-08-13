c60df6948b0be1ca26304bc324f2d96e1afae8d9
nickwlong <nickwlong@gmail.com>
2022-08-13 08:31:31 +0100

    Added basket functionality and terminal input options

--- lib/Customer.rb
+++ lib/Customer.rb
@@ -2,20 +2,69 @@ require_relative "./Menu.rb"
 require_relative "./Menu_reader.rb"
 
 Customer = Struct.new(:contactname, :contactaddress, :contactphonenumber) do
-  def initialize(io=Kernel)
+  def initialize(menureader, io=Kernel)
     @basket = []
+    @menureader = menureader
+    @menu = @menureader.menu
+    @io = io
+    @input_choice = ""
   end
 
   def run
+    self.welcome_customer_details
+    @menureader.print_menu
+    unless @input_choice == "print receipt"
+      self.customer_terminal_choice
+    end
+  end
+  
+  def add_item_to_basket
+    @io.puts "Please type the name of the dish you wish to add exactly as found in the menu:"
+    dish = @io.gets.chomp
+    @io.puts "How many of this dish would you like? Please type a number"
+    quantity = @io.gets.chomp.to_i
+  end
+
+  def print_receipt
+  end
+  
+  private
+
+  def welcome_customer_details
     @io.puts "Welcome to the takeaway service for Hotel Nicholas, please enter your name:"
     @contactname = @io.gets.chomp
-    "Please enter your address"
+    @io.puts "\nWelcome, #{@contactname}, please enter your address:"
     @contactaddress = @io.gets.chomp
-    "Please enter your contact number"
+    @io.puts "\nPlease enter your contact number:"
     @contactphonenumber = @io.gets.chomp
-    
   end
+
+  def customer_terminal_choice
+    @io.puts "Please type 'menu' to view the menu again, or 'add dish' to start adding dishes to your basket."
+    @input_choice = @io.gets.chomp
+    if @input_choice == 'menu'
+      @menureader.print_menu
+    elsif @input_choice == 'add dish'
+      self.add_item_to_basket
+    elsif @input_choice == 'quit'
+      self.quit
+    end
+  end  
+
 end
 
-nick = Customer.new
+hotelNicholas = Menu.new
+hotelNicholas.add_dish("Onion Soup", "6.50", "starter")
+hotelNicholas.add_dish("Carrot Soup", "6.50", "starter")
+hotelNicholas.add_dish("Brixham Crab", "11.50", "starter")
+hotelNicholas.add_dish("Hand Dived Devon Scallop", "10.25", "starter")
+hotelNicholas.add_dish("Rump of New Season Lamb", "16.50", "main")
+hotelNicholas.add_dish("Devon Blue Croquettes", "14.00", "main")
+hotelNicholas.add_dish("Dry Aged Beef Fillet", "27.50", "main")
+hotelNicholas.add_dish("Roasted Monkfish", "16.50", "main")
+hotelNicholas.add_dish("Apricot Sorbet", "6.00", "dessert")
+hotelNicholas.add_dish("Honey, Whisky and Almond Sponge", "8.00", "dessert")
+hotelNicholas.add_dish("Chocolate Arctic Roll", "7.00", "dessert")
+hotelNick = MenuReader.new(hotelNicholas)
+nick = Customer.new(hotelNick)
 nick.run
--- lib/Menu_reader.rb
+++ lib/Menu_reader.rb
@@ -1,6 +1,7 @@
 require_relative './Menu'
 
 class MenuReader
+    attr_accessor :menu
     def initialize(menu, io = Kernel)
         @menu = menu
         @io = io
@@ -34,7 +35,7 @@ class MenuReader
 
     def print_footer
         @io.puts "\n\n"
-        @io.puts "To place an order, please type the item \nname exactly as found in the menu. \nThank you for visiting".center(40)
+        @io.puts "**To place an order, please type the item \nname exactly as found in the menu.** \nThank you for visiting".center(40)
         @io.puts "------------------------------------------------\n\n"
     end
 
@@ -50,18 +51,4 @@ class MenuReader
         self.print_footer
     end
 end
-hotelNicholas = Menu.new
-hotelNicholas.add_dish("Onion Soup", "6.50", "starter")
-hotelNicholas.add_dish("Carrot Soup", "6.50", "starter")
-hotelNicholas.add_dish("Brixham Crab", "11.50", "starter")
-hotelNicholas.add_dish("Hand Dived Devon Scallop", "10.25", "starter")
-hotelNicholas.add_dish("Rump of New Season Lamb", "16.50", "main")
-hotelNicholas.add_dish("Devon Blue Croquettes", "14.00", "main")
-hotelNicholas.add_dish("Dry Aged Beef Fillet", "27.50", "main")
-hotelNicholas.add_dish("Roasted Monkfish", "16.50", "main")
-hotelNicholas.add_dish("Apricot Sorbet", "6.00", "dessert")
-hotelNicholas.add_dish("Honey, Whisky and Almond Sponge", "8.00", "dessert")
-hotelNicholas.add_dish("Chocolate Arctic Roll", "7.00", "dessert")
-hotelNick = MenuReader.new(hotelNicholas)
 
-p hotelNick.print_menu
